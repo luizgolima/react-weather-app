cf69af870f84c112709f4c80bb90e1524509b08f
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-24 17:33:05 -0300

    establish API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
-import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation.jsx";
 import TemperatureAndDetails from "./components/TemperatureAndDetails.jsx";
 import Forecast from "./components/Forecast.jsx";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
