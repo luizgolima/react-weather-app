2c3a6507ad04a10c76ce3236746a909da4621923
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-24 14:53:31 -0300

    add Inputs component

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 
 function TopButtons() {
-    const cities = [{
-        id: 1, title: "London"
-    }, {
-        id: 2, title: "Paris"
-    }, {
-        id: 3, title: "Berlin"
-    }, {
-        id: 4, title: "Toronto"
-    }, {
-        id: 5, title: "New York"
-    }];
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "Paris",
+    },
+    {
+      id: 3,
+      title: "Berlin",
+    },
+    {
+      id: 4,
+      title: "Toronto",
+    },
+    {
+      id: 5,
+      title: "New York",
+    },
+  ];
 
-    return (<div className="flex items-center justify-around my-6">
-        {cities.map((city) => (<button key={city.id} className="text-white text-lg font-medium">
-            {city.title}
-        </button>))}
-    </div>);
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default TopButtons;
