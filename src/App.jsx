eb7b57132774d9816e48c5769c86053d07b1dfa8
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-24 13:54:25 -0300

    add TopButtons component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons'
 
 function App() {
 
-  return (
-    <div className="App">
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-        <UilReact className="text-6xl" />
-    </div>
-  )
+    return (
+        <div
+            className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+            <TopButtons/>
+        </div>
+    )
 }
 
 export default App
