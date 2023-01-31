b495dab8504246e835240a8ceb33849616413262
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-31 13:27:17 -0300

    Update TopButtons.jsx

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -25,17 +25,17 @@ function TopButtons({ setQuery }) {
   ];
 
   return (
-    <div className="flex items-center justify-around my-6">
-      {cities.map((city) => (
-        <button
-          key={city.id}
-          className="text-white text-lg font-medium"
-          onClick={() => setQuery({ q: city.title })}
-        >
-          {city.title}
-        </button>
-      ))}
-    </div>
+      <div className="flex items-center justify-around my-6">
+        {cities.map((city) => (
+            <button
+                key={city.id}
+                className="text-white text-lg font-medium"
+                onClick={() => setQuery({ q: city.title })}
+            >
+              {city.title}
+            </button>
+        ))}
+      </div>
   );
 }
 
